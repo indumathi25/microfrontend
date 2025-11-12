4a759980e68eae4a34cb16b2f12e4f01ca479748
indumathi25 <ganga.indumathi19@gmail.com>
2025-11-12 15:19:35 +0100

    Resolved default region

--- packages/auth/src/App.js
+++ packages/auth/src/App.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import { Switch, Route, Router } from 'react-router-dom';
-import {
-  StylesProvider,
-  createGenerateClassName,
-} from '@material-ui/core/styles';
+import { StylesProvider, createGenerateClassName } from '@mui/material/styles';
 
 import Signin from './components/Signin';
 import Signup from './components/Signup';
@@ -18,10 +15,10 @@ export default ({ history, onSignIn }) => {
       <StylesProvider generateClassName={generateClassName}>
         <Router history={history}>
           <Switch>
-            <Route path="/auth/signin">
+            <Route path='/auth/signin'>
               <Signin onSignIn={onSignIn} />
             </Route>
-            <Route path="/auth/signup">
+            <Route path='/auth/signup'>
               <Signup onSignIn={onSignIn} />
             </Route>
           </Switch>
--- packages/marketing/src/App.js
+++ packages/marketing/src/App.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import { Switch, Route, Router } from 'react-router-dom';
-import {
-  StylesProvider,
-  createGenerateClassName,
-} from '@material-ui/core/styles';
+import { StylesProvider, createGenerateClassName } from '@mui/styles';
 
 import Landing from './components/Landing';
 import Pricing from './components/Pricing';
@@ -18,8 +15,8 @@ export default ({ history }) => {
       <StylesProvider generateClassName={generateClassName}>
         <Router history={history}>
           <Switch>
-            <Route exact path="/pricing" component={Pricing} />
-            <Route path="/" component={Landing} />
+            <Route exact path='/pricing' component={Pricing} />
+            <Route path='/' component={Landing} />
           </Switch>
         </Router>
       </StylesProvider>
